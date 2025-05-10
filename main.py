ba44bd0228970f2db01b23c80563504ff47a3a32
kislay-yadav <sahilbhau148@gmail.com>
2025-05-10 21:40:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,6 @@ def index():
             return render_template("index.html", error=str(e))
     return render_template("index.html", error=None)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
