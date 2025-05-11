e67967182b2305db5280e73d4ea9aa6396d4ac4b
kislay-yadav <sahilbhau148@gmail.com>
2025-05-11 11:08:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pytube import YouTube
 import os
 
 app = Flask(__name__)
+
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
@@ -12,13 +13,13 @@ def index():
         url = request.form["url"]
         try:
             yt = YouTube(url)
-            stream = yt.streams.get_highest_resolution()
-            file_path = stream.download(output_path=DOWNLOAD_FOLDER)
-            return send_file(file_path, as_attachment=True)
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            filepath = stream.download(output_path=DOWNLOAD_FOLDER)
+            filename = os.path.basename(filepath)
+            return send_file(filepath, as_attachment=True)
         except Exception as e:
             return render_template("index.html", error=str(e))
-    return render_template("index.html", error=None)
-
+    return render_template("index.html")
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
